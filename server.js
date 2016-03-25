eb6eb145a9d2d26e0b44609f978cc226b81efbd9
tyler liu <replicat0r.misc@gmail.com>
2016-03-25 11:22:22 -0400

    added front end form to submit message

--- server.js
+++ server.js
@@ -6,21 +6,19 @@ var io = require('socket.io')(http)
 
 app.use(express.static(__dirname + '/public'))
 
-io.on('connection',function(socket){
-    console.log('user connect via sockets')  
+io.on('connection', function(socket) {
+    console.log('user connect via sockets')
     socket.emit('message',{
         text: "welcome to the chat application"
     })
     
-    socket.on('disconnect',function(){
-        console.log('user has disconnected')
+    socket.on('message',function(message){
+        console.log('Message Received '+message.text)
+        socket.emit(message.text)
     })
-    
-    socket.on('update',function(){
-        
-    })
+   
 })
 
-http.listen(PORT,function(){
+http.listen(PORT, function() {
     console.log(`Server started on ${PORT}`)
 })
