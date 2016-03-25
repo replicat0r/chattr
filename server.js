92c24233aa7eeebbebe301aff8cba0efec934470
tyler liu <replicat0r.misc@gmail.com>
2016-03-25 17:50:18 -0400

    refactored js, list all user in a chat room

--- server.js
+++ server.js
@@ -8,15 +8,33 @@ var moment = require('moment')
 app.use(express.static(__dirname + '/public'))
 var clientInfo = {}
 
+function sendCurrentUsers(socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+    if (typeof info === 'undefined') {
+        return;
+    }
+
+    Object.keys(clientInfo).forEach(function(socketId) {
+        var userInfo = clientInfo[socketId]
+        if (info.room === userInfo.room) {
+            users.push(userInfo.room)
+        }
+    })
+    socket.emit('message', {
+        name: "System",
+        text: `Current user: ${users.join(', ')}`
+    })
+}
 
 io.on('connection', function(socket) {
     console.log('user connect via sockets')
-    
-    socket.on('disconnect',function(){
+
+    socket.on('disconnect', function() {
         var userData = clientInfo[socket.id]
-        if (typeof userData !== 'undefined'){
+        if (typeof userData !== 'undefined') {
             socket.leave(userData.room);
-            io.to(userData.room).emit('message',{
+            io.to(userData.room).emit('message', {
                 name: "System",
                 text: `${userData.name} has left!`,
                 timeStamp: moment.valueOf()
@@ -24,15 +42,16 @@ io.on('connection', function(socket) {
             delete clientInfo[socket.id];
         }
     })
-    
-    socket.on('joinRoom',function(req){
+
+    socket.on('joinRoom', function(req) {
         clientInfo[socket.id] = req;
         socket.join(req.room);
-        socket.broadcast.to(req.room).emit('message',{
+        socket.broadcast.to(req.room).emit('message', {
             name: 'System',
             text: `${req.name} has joined!`,
             timeStamp: moment().valueOf()
         })
+        sendCurrentUsers(socket)
     })
     socket.emit('message', {
         text: "welcome to the chat application",
@@ -42,9 +61,12 @@ io.on('connection', function(socket) {
     })
 
     socket.on('message', function(message) {
-        
+
         message.timeStamp = moment.valueOf()
         io.to(clientInfo[socket.id].room).emit('message', message)
+
+
+
     })
 
 
