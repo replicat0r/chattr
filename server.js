2f077118a329171529ec07d992e830476a1a0428
tyler liu <replicat0r.misc@gmail.com>
2016-03-25 10:42:49 -0400

    added socket io to app;

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ var PORT = process.env.PORT || 3000
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname + '/public'))
+
+io.on('connection',function(socket){
+    console.log('user connect via sockets')  
+    socket.emit('message',{
+        text: "welcome to the chat application"
+    })
+    
+    socket.on('disconnect',function(){
+        console.log('user has disconnected')
+    })
+    
+    socket.on('update',function(){
+        
+    })
+})
+
 http.listen(PORT,function(){
-    console.log('Server Start')
+    console.log(`Server started on ${PORT}`)
 })
