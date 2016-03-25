59ab1ebee914229abd722fc73b7d0ad00bc1f4e8
tyler liu <replicat0r.misc@gmail.com>
2016-03-25 11:52:09 -0400

    add message to browser UI

--- server.js
+++ server.js
@@ -8,15 +8,16 @@ app.use(express.static(__dirname + '/public'))
 
 io.on('connection', function(socket) {
     console.log('user connect via sockets')
-    socket.emit('message',{
+    socket.emit('message', {
         text: "welcome to the chat application"
     })
-    
-    socket.on('message',function(message){
-        console.log('Message Received '+message.text)
-        socket.emit(message.text)
+
+    socket.on('message', function(message) {
+        console.log('Message Received on server: ' + message.text)
+        io.emit('message', message)
     })
-   
+
+
 })
 
 http.listen(PORT, function() {
